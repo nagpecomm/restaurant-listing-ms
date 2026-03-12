ad59f338cb99c833dce9b12491b1adb21b974071
Mahesh Tambe <maheshtambelab@gmail.com>
2026-03-12 19:25:48 +0530

    search service

--- src/main/java/com/codeddecode/restaurantlisting/RestaurantListingApplication.java
+++ src/main/java/com/codeddecode/restaurantlisting/RestaurantListingApplication.java
@@ -2,12 +2,21 @@ package com.codeddecode.restaurantlisting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class RestaurantListingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestaurantListingApplication.class, args);
+	}
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
 	}
 
 }
--- src/main/java/com/codeddecode/restaurantlisting/service/RestaurantService.java
+++ src/main/java/com/codeddecode/restaurantlisting/service/RestaurantService.java
@@ -5,36 +5,58 @@ import com.codeddecode.restaurantlisting.dto.RestaurantDTO;
 import com.codeddecode.restaurantlisting.entity.Restaurant;
 import com.codeddecode.restaurantlisting.mapper.RestaurantMapper;
 import com.codeddecode.restaurantlisting.repo.RestaurantRepo;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Service
 public class RestaurantService {
 
     @Autowired
     RestaurantRepo restaurantRepo;
 
+    @Autowired
+    RestTemplate restTemplate;
+
 
     public List<RestaurantDTO> findAllRestaurants() {
         List<Restaurant> restaurants = restaurantRepo.findAll();
         List<RestaurantDTO> restaurantDTOList = restaurants.stream().map(restaurant -> RestaurantMapper.INSTANCE.mapRestaurantToRestaurantDTO(restaurant)).collect(Collectors.toList());
+
+        try {
+
+            String url = "http://SEARCH-SERVICE/api/products/search?keyword=&page=0&size=10";
+
+            ResponseEntity<Object> response = restTemplate.getForEntity(
+                    url,
+                    Object.class
+            );
+
+            log.info("Status: {}", response.getStatusCode());
+            log.info("Body: {}", response.getBody());
+
+        } catch (Exception e) {
+            log.error("Exception while calling search service", e);
+        }
         return restaurantDTOList;
     }
 
     public RestaurantDTO addRestaurantInDB(RestaurantDTO restaurantDTO) {
-        Restaurant savedRestaurant =  restaurantRepo.save(RestaurantMapper.INSTANCE.mapRestaurantDTOToRestaurant(restaurantDTO));
+        Restaurant savedRestaurant = restaurantRepo.save(RestaurantMapper.INSTANCE.mapRestaurantDTOToRestaurant(restaurantDTO));
         return RestaurantMapper.INSTANCE.mapRestaurantToRestaurantDTO(savedRestaurant);
     }
 
     public ResponseEntity<RestaurantDTO> fetchRestaurantById(Integer id) {
-        Optional<Restaurant> restaurant =  restaurantRepo.findById(id);
-        if(restaurant.isPresent()){
+        Optional<Restaurant> restaurant = restaurantRepo.findById(id);
+        if (restaurant.isPresent()) {
             return new ResponseEntity<>(RestaurantMapper.INSTANCE.mapRestaurantToRestaurantDTO(restaurant.get()), HttpStatus.OK);
         }
         return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
